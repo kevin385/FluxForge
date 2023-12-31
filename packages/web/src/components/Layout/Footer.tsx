8d12b4ded3375931d1546fb30705a155d4b80261
Kevin Biju <kevibk@gmail.com>
2023-12-31 23:21:04 +0530

    added tailwind and added header and footer

---
+++ packages/web/src/components/Layout/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <p>&copy; {new Date().getFullYear()} FluxForge</p>
+    </footer>
+  );
+};
---
+++ packages/web/src/components/Layout/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+      <header className="bg-gray-800 text-white p-4">
+        <h1>FluxForge</h1>
+      </header>
+    );
+  };
+  
