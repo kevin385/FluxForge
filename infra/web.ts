fc1fcea3db0332fc2f47931f9fe3ca0bf3b712b3
Kevin Biju <kevibk@gmail.com>
2024-12-29 21:14:41 +0530

    reverted web.ts

--- infra/web.ts
+++ infra/web.ts
@@ -1,31 +1,8 @@
-// export const web = (api: sst.cloudflare.Worker) =>
-//   new sst.x.DevCommand("Web", {
-//     link: [api],
-//     dev: {
-//       autostart: true,
-//       command: "pnpm dev:web"
-//     }
-//   });
-
-export const web = () =>
-  new cloudflare.PagesProject("Web", {
-    accountId: sst.cloudflare.DEFAULT_ACCOUNT_ID,
-    name: "fluxforge-web-app",
-    productionBranch: "main",
-    // Build configuration
-    buildConfig: {
-      rootDir: "./packages/web",
-      buildCommand: "pnpm run build",
-      destinationDir: "out"
-    },
-    deploymentConfigs: {
-      production: {
-        secrets: {},
-        failOpen: false
-      },
-      preview: {
-        secrets: {},
-        failOpen: false
-      }
+export const web = (api: sst.cloudflare.Worker) =>
+  new sst.x.DevCommand("Web", {
+    link: [api],
+    dev: {
+      autostart: true,
+      command: "pnpm dev:web"
     }
   });
